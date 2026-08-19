5566935d7d91538b5348cae5c96ff7a02bcb688f
Antor <alantorbd@gmail.com>
2026-08-19 16:22:33 +0600

    complete sidebar design

---
+++ client/src/pages/AIComposer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AIComposer() {
+  return <div>AIComposer</div>;
+}
---
+++ client/src/pages/Scheduler.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Scheduler() {
+  return <div>Scheduler</div>;
+}
